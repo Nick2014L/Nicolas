45c9c98ad568aa3db525d8d57712f4be738f50b0
Nick2014L <nicolas.arlo.leonard@gmail.com>
2026-07-15 13:20:41 -0400

    css code

--- todolist/todo.js
+++ todolist/todo.js
@@ -1,6 +1,9 @@
 function addItem() {
-    const list=document.getElementById("list")
-    const newitem=document.createElement("p")
-    newitem.textContent=document.getElementById("new-items").value
-    list.appendChild(newitem)
+    if (document.getElementById("new-items").value!= ""){
+        const list=document.getElementById("list")
+        const newitem=document.createElement("p")
+        newitem.textContent=document.getElementById("new-items").value
+        list.appendChild(newitem)
+        document.getElementById("new-items").value=""
+    }
 }
