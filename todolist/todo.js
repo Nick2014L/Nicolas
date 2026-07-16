b04adb89aa710a01be882a18e93974aca8bd8bb7
Nick2014L <nicolas.arlo.leonard@gmail.com>
2026-07-16 09:50:35 -0400

    added delete

--- todolist/todo.js
+++ todolist/todo.js
@@ -1,9 +1,23 @@
 function addItem() {
     if (document.getElementById("new-items").value!= ""){
         const list=document.getElementById("list")
-        const newitem=document.createElement("p")
-        newitem.textContent=document.getElementById("new-items").value
+        const newp=document.createElement("p")
+        const newimage=document.createElement("img")
+        newp.textContent=document.getElementById("new-items").value
+        newimage.src="delete.svg"
+        const newitem=document.createElement("div")
+        newitem.appendChild(newp)
+        newitem.appendChild(newimage)
+        newimage.addEventListener('click', (event)=>{
+            newitem.remove()
+        })
         list.appendChild(newitem)
         document.getElementById("new-items").value=""
     }
 }
+
+document.getElementById("new-items").addEventListener("keyup", (event) => {
+    if (event.key=="Enter") {
+        addItem()
+    }
+ })
