469f18a54e21e077dd507ceb0350c7411b40807e
Nick2014L <nicolas.arlo.leonard@gmail.com>
2026-07-16 11:04:18 -0400

    local storage fix

--- todolist/todo.js
+++ todolist/todo.js
@@ -1,5 +1,14 @@
 function addItem() {
     if (document.getElementById("new-items").value!= ""){
+        var storage = []
+        if (localStorage.getItem("list")===null){
+            storage=[]
+        }
+        else{
+            storage=JSON.parse(localStorage.getItem("list"))
+        }
+        storage.push(document.getElementById("new-items").value)
+        localStorage.setItem("list", JSON.stringify(storage))
         const list=document.getElementById("list")
         const newp=document.createElement("p")
         const newimage=document.createElement("img")
@@ -10,6 +19,9 @@ function addItem() {
         newitem.appendChild(newimage)
         newimage.addEventListener('click', (event)=>{
             newitem.remove()
+            const stored=JSON.parse(localStorage.getItem("list"))
+            stored.splice(stored.indexOf(newp.textContent), 1)
+            localStorage.setItem("list", JSON.stringify(stored))
         })
         list.appendChild(newitem)
         document.getElementById("new-items").value=""
@@ -21,3 +33,24 @@ document.getElementById("new-items").addEventListener("keyup", (event) => {
         addItem()
     }
  })
+
+document.addEventListener("DOMContentLoaded", (event)=>{
+    const storage=JSON.parse(localStorage.getItem("list"))
+    storage.forEach(element => {
+        const list=document.getElementById("list")
+        const newp=document.createElement("p")
+        const newimage=document.createElement("img")
+        newp.textContent=element
+        newimage.src="delete.svg"
+        const newitem=document.createElement("div")
+        newitem.appendChild(newp)
+        newitem.appendChild(newimage)
+        newimage.addEventListener('click', (event)=>{
+            newitem.remove()
+            const stored=JSON.parse(localStorage.getItem("list"))
+            stored.splice(stored.indexOf(newp.textContent), 1)
+            localStorage.setItem("list", JSON.stringify(stored))
+        })
+        list.appendChild(newitem)
+    });
+})
